11820c533b0a2b966a21cdd461f35eebbb56be2c
saldaykin1@gmail.com <15192328Sald>
2021-12-08 17:44:32 +0300

    print1fghj

--- 123.py
+++ 123.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('qqeq')
-    print('qfdnvdfvhev')
+    print('qfdnvdfvheve')
 
 
 if __name__ == '__main__':
